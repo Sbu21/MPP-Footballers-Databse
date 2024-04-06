082a1a01c46e2751936646e9a2f1c7657c8789a5
MihneaS16 <seitoarumihnea@gmail.com>
2024-04-06 15:23:18 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 const app = express();
 const carRoutes = require('./routes/carRoutes.js');
@@ -20,6 +20,7 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+app.use(cors());
 
 app.use('/cars', carRoutes);
 
